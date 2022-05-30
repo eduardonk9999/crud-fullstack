573a38642387dd3e8378053b0c46d7f3c52a3506
eduardonk9999 <thiago.novak7@gmail.com>
2022-05-30 16:07:40 -0300

    start back

---
+++ comandos.md
@@ -0,0 +1,8 @@
+# CRUD FullStack
+
+<ul>
+  <li>React</li>
+  <li>Node / Express</li>
+  <li>GraphQL</li>
+  <li>MySQL</li>
+</ul>
---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { schema } from './Schema'
+import cors from 'cors'
+import { DataSource } from 'typeorm'
+
+const main = async () => {
+  await new DataSource({
+    type: "mysql",
+    database: "Docker-CRUD-FULLSTACK",
+    username: "root",
+    password: "root",
+    logging: true,
+    synchronize: false,
+    entities: [],
+  })
+
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+  app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
+  }))
+
+  app.listen(3001, () => {
+    console.log("SERVER PORT 3001 :rocket:")
+  })
+}
+
+main().catch((err => {
+  console.log(err)
+}))
